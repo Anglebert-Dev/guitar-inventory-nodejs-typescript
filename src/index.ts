a597c726d63f90b2e323ac85a6cce3a076a8c881
Anglebert-Dev <anglebertsh@gmail.com>
2022-08-20 12:12:02 +0200

    built user interface with ejs and materialize

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import express from  "express"
+import path from "path"
 
 const app  = express()
 const port = 6000
 
+//  configure Express to use EJS
+app.set("views",path.join(__dirname , "views"));
+app.set("view engine" , "ejs");
+
+// default home page route
 app.get(("/") , (req,res)=>{
-    res.send("hello world");
+    // rendering the index template
+    res.render("index");
 })
 
 app.listen(port , ()=>{
