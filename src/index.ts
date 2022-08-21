43a3455a8888cbdf6286ca99220f221cacc8e2c8
Anglebert-Dev <anglebertsh@gmail.com>
2022-08-21 02:16:59 +0200

    added view & removed node modules

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,24 @@
-import express from  "express"
-import path from "path"
+import dotenv from "dotenv";
+import express from "express";
+import path from "path";
+const app = express();
 
-const app  = express()
-const port = 8080
 
+// initialize config..
+dotenv.config();
+
+const port = process.env.SERVER_PORT;
 //  configure Express to use EJS
-app.set("views",path.join(__dirname , "views"));
-app.set("view engine" , "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // default home page route
-app.get(("/") , (req,res)=>{
-    // rendering the index template
-    res.render("index");
-})
+app.get("/", (req, res) => {
+  // rendering the index template
+  res.render("index");
+});
 
-app.listen(port , ()=>{
-    // tslint:disable-next-line:no-console
-    console.log(`app listening on http://localhost:${port}` );
-})
+app.listen(port, () => {
+  // tslint:disable-next-line:no-console
+  console.log(`app listening on http://localhost:${port}`);
+});
